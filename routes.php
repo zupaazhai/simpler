d9a8fe268da3e5350284121b171d5781ce2bf3d4
Supachai Wongmoon <supachai@outlook.com>
2019-08-17 22:17:22 +0700

    Wrap with admin

--- routes.php
+++ routes.php
@@ -7,4 +7,7 @@ Flight::route('GET /login', array(new UserController, 'loginForm'));
 Flight::route('POST /login', array(new UserController, 'login'));
 Flight::route('POST /new-user', array(new UserController, 'create'));
 
-Flight::route('/dashboard', array(new DashboardController, 'index'));
+Flight::map('admin', function () {
+    Flight::route('/dashboard', array(new DashboardController, 'index'));
+});
+Flight::admin();
